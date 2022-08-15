0f4eb9218ec6634dd5ea4fcd3f47361bbb297139
Cedric Honnet <honnet@telecom-paristech.org>
2022-08-15 16:12:03 -0700

    Fix I2C buffer bug.

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,139 +1,85 @@
-#include <Zforce.h>
-#define DATA_READY 2
+// using this lib to solve I2C buffer problem: https://github.com/stephaneAG/zforce-arduino
+// Note: uncomment "#define USE_I2C_LIB 1" in Zforce.cpp if using official SAMD21-based NeoNode board
+
+#include <Zforce.h> // R: modded version including I2C lib or modified "MYWire" lib
+#define PIN_NN_DR 7 // INT6
+#define PIN_NN_RST 5
+
 
 //#define DEBUG_PRINT
 
-
-void setup()
-{
-  Serial.begin(115200);
-  while (!Serial) {};
-  zforce.Start(DATA_READY);
-  init_sensor();
-}
-
-void loop()
-{
-  Message* touch = zforce.GetMessage();
-  if (touch != NULL)
-  {
-    if (touch->type == MessageType::TOUCHTYPE)
-    {
-      //for (uint8_t i = 0; i < 1; i++)
-      for (uint8_t i = 0; i < ((TouchMessage*)touch)->touchCount; i++)
-      {
-
 #ifdef DEBUG_PRINT
-        Serial.print(((TouchMessage*)touch)->touchData[i].id);
-        Serial.print("\t");
+    #define Serial_print(...)     Serial.print(__VA_ARGS__)
+    #define Serial_println(...)   Serial.println(__VA_ARGS__)
+#else
+    #define Serial_print(...)
+    #define Serial_println(...)
 #endif
 
-        unsigned x = ((TouchMessage*)touch)->touchData[i].x;
-        unsigned y = ((TouchMessage*)touch)->touchData[i].y;
 
-        if (x != 0 && y != 0) {
-          // TODO: add id
-          char buf[17];
-          sprintf(buf, "%04u.%04u\n", x, y);
-          Serial.print(buf);
+void setup() {
+    pinMode(PIN_NN_RST, OUTPUT);            // reset pin
+    pinMode(PIN_NN_DR, INPUT);              // data ready
+    zforce.Start(PIN_NN_DR);
+
+    digitalWrite(PIN_NN_RST, LOW);          // Reset sensor
+    delay(10);
+    digitalWrite(PIN_NN_RST, HIGH);
+
+    Serial.begin(115200);
+    while(!Serial){};
+    Serial_println("zforce start");
+
+    // Init / Boot complete
+    Message* msg = zforce.GetMessage();
+    do { msg = zforce.GetMessage(); } while (msg == NULL);
+    if (msg != NULL){
+        Serial_println("Received Boot Complete Notification");
+        Serial_print("Message type is: ");
+        Serial_println((int)msg->type);
+        zforce.DestroyMessage(msg);
+    }
+
+    // Send and read Enable - mandatory
+    Serial_println("sending enable ..");
+    zforce.Enable(true);
+    do { msg = zforce.GetMessage(); } while (msg == NULL);
+    if (msg->type == MessageType::ENABLETYPE){
+        Serial_print("Message type is: ");
+        Serial_println((int)msg->type);
+        Serial_println("Sensor is now enabled and will report touches.");
+    }
+    zforce.DestroyMessage(msg);
+}
+
+void loop() {
+
+    // receives touch updates via polling
+    Message* touch = zforce.GetMessage();
+    if (touch != NULL){
+        Serial_println("Touch Msg");
+        if (touch->type == MessageType::TOUCHTYPE){
+            for (uint8_t i = 0; i < ((TouchMessage*)touch)->touchCount; i++){
+
+                Serial_print("ID is: ");
+                Serial_println(((TouchMessage*)touch)->touchData[i].id);
+
+                Serial_print("X.Y: ");
+                unsigned x = ((TouchMessage*)touch)->touchData[i].x;
+                unsigned y = ((TouchMessage*)touch)->touchData[i].y;
+
+                if (x != 0 && y != 0) {
+                    // TODO: add id
+                    char buf[17];
+                    sprintf(buf, "%04u.%04u\n", x, y);
+                    Serial.print(buf);
+                }
+
+                Serial_print("Event is: ");
+                Serial_println(((TouchMessage*)touch)->touchData[i].event);
+            }
         }
-      }
+
+        zforce.DestroyMessage(touch);
     }
-    else if (touch->type == MessageType::BOOTCOMPLETETYPE)
-    {
-      /*If we for some reason we would receive a boot
-        complete message, the sensor needs to be reinitiated.*/
-      init_sensor();
-    }
-
-    zforce.DestroyMessage(touch);
-  }
-}
-
-
-void init_sensor()
-{
-  Message *msg = NULL;
-
-  // Send and read ReverseX
-  zforce.ReverseX(false);
-
-  do
-  {
-    msg = zforce.GetMessage();
-  } while (msg == NULL);
-
-#ifdef DEBUG_PRINT
-  if (msg->type == MessageType::REVERSEXTYPE)
-  {
-    Serial.println("Received ReverseX Response");
-    Serial.print("Message type is: ");
-    Serial.println((int)msg->type);
-  }
-#endif
-
-  zforce.DestroyMessage(msg);
-
-  // Send and read ReverseY
-  zforce.ReverseY(false);
-
-  do
-  {
-    msg = zforce.GetMessage();
-  } while (msg == NULL);
-
-#ifdef DEBUG_PRINT
-  if (msg->type == MessageType::REVERSEYTYPE)
-  {
-    Serial.println("Received ReverseY Response");
-    Serial.print("Message type is: ");
-    Serial.println((int)msg->type);
-  }
-#endif
-
-  zforce.DestroyMessage(msg);
-
-  // Send and read Touch Active Area
-  zforce.TouchActiveArea(0, 0, 4000, 4000);
-
-  do
-  {
-    msg = zforce.GetMessage();
-  } while (msg == NULL);
-
-#ifdef DEBUG_PRINT
-  if (msg->type == MessageType::TOUCHACTIVEAREATYPE)
-  {
-    Serial.print("minX is: ");
-    Serial.println(((TouchActiveAreaMessage *)msg)->minX);
-    Serial.print("minY is: ");
-    Serial.println(((TouchActiveAreaMessage *)msg)->minY);
-    Serial.print("maxX is: ");
-    Serial.println(((TouchActiveAreaMessage *)msg)->maxX);
-    Serial.print("maxY is: ");
-    Serial.println(((TouchActiveAreaMessage *)msg)->maxY);
-  }
-#endif
-
-  zforce.DestroyMessage(msg);
-
-  // Send and read Enable
-
-  zforce.Enable(true);
-
-  do
-  {
-    msg = zforce.GetMessage();
-  } while (msg == NULL);
-
-#ifdef DEBUG_PRINT
-  if (msg->type == MessageType::ENABLETYPE)
-  {
-    Serial.print("Message type is: ");
-    Serial.println((int)msg->type);
-    Serial.println("Sensor is now enabled and will report touches.");
-  }
-#endif
-
-  zforce.DestroyMessage(msg);
 }
--- viz/viz.pde
+++ viz/viz.pde
@@ -28,16 +28,14 @@ void draw() {
     while (myPort.available() > 0) {
 
         receivedString = myPort.readStringUntil('\n');
-        if (receivedString != null) {
+        println(receivedString);
 
-            if (receivedString.length() == 10) {
-
-                if (receivedString.charAt(4) == '.') {
-                    xy[0] = Float.parseFloat(receivedString.substring(0, 4));
-                    xy[1] = Float.parseFloat(receivedString.substring(5, 9));
-                }
-            }
-            println(xy[0], xy[1]);
+        if (receivedString != null &&
+            receivedString.length() == 10 &&
+            receivedString.charAt(4) == '.')
+        {
+            xy[0] = Float.parseFloat(receivedString.substring(0, 4));
+            xy[1] = Float.parseFloat(receivedString.substring(5, 9));
 
             xy[0] = map(xy[0] ,  0, xy_max[0] ,  0.0, float(width));
             xy[1] = map(xy[1] ,  0, xy_max[1] ,  0.0, float(height));
